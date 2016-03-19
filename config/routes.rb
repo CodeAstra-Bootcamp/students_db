c1b576ef792a3d85e6a7d9f00052a5174ae8ad60
B V Satyaram <bvsatyaram@gmail.com>
2016-03-19 12:48:03 +0530

    Show max score along with the students score

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :attendance_registries
     end
   end
-  resources :exams, only: [:new, :create]
+  resources :exams, only: [:index, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
