c00de1df845a4c276228eb5e5266ee362cbcb1ff
B V Satyaram <bvsatyaram@gmail.com>
2016-03-19 11:10:22 +0530

    Remove config details for twilio

--- lib/messenger_service.rb
+++ lib/messenger_service.rb
@@ -4,8 +4,8 @@ require 'twilio-ruby'
 class MessengerService
   def initialize
     # put your own credentials here
-    account_sid = 'AC12077148dd63a5d79e5e2531a1fdcbcb'
-    auth_token = 'd184cc144faeb61d5585df80cc4bffea'
+    account_sid = 'XXX'
+    auth_token = 'YYY'
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
