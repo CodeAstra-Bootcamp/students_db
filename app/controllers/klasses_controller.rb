c14ef4e0afdd3bde9681f44f5bdf787aac82716f
B V Satyaram <bvsatyaram@gmail.com>
2016-03-22 17:49:46 +0530

    Add Pie-chart for grade distribution

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -48,5 +48,32 @@ class KlassesController < ApplicationController
       end
       @avg_marks_data.push({name: "Section: #{section.name}", data: data})
     end
+
+    # Grades Distribution Data
+    sections = @klass.sections.includes(students: [:exam])
+    students = sections.collect {|section| section.students}.flatten
+    marks = students.collect(&:exam).collect{|ex| ex.total/5}
+    data = {s: 0, a: 0, b: 0, c: 0, d:0, e: 0, f: 0}
+    marks.each do |mark|
+      if mark >= 90
+        data[:s] += 1
+      elsif mark >= 80
+        data[:a] += 1
+      elsif mark >= 70
+        data[:b] += 1
+      elsif mark >= 60
+        data[:c] += 1
+      elsif mark >= 50
+        data[:d] += 1
+      elsif mark >= 40
+        data[:e] += 1
+      else
+        data[:f] += 1
+      end
+    end
+    @grades_distribution_data = {}
+    data.each do |key, val|
+      @grades_distribution_data["Grade #{key.to_s.upcase}"] = val
+    end
   end
 end
