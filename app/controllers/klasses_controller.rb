4a45d44aa60a3b6bb3567bbe3c1d878ce4e844ee
B V Satyaram <bvsatyaram@gmail.com>
2016-05-27 20:54:19 +0530

    Average scores colum chart

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -27,8 +27,9 @@ class KlassesController < ApplicationController
     attendance_registries = AttendanceRegistry.where('date >= ?', Date.today - 14.days).where(section_id: sections.pluck(:id))
     grouped_attendance_registries = attendance_registries.group_by(&:section_id)
     absentees = Absentee.where(attendance_registry_id: attendance_registries.pluck(:id)).group_by(&:attendance_registry_id)
+    exams = Exam.where(student_id: students.values.flatten.collect(&:id))
 
-    @data = []
+    @attendance_data = []
     sections.each do |section|
       data = {}
       students_count = students[section.id].length
@@ -36,7 +37,26 @@ class KlassesController < ApplicationController
         absentees_count = (absentees[ar.id] || []).length
         data[ar.date] = 100*(students_count - absentees_count)/students_count
       end
-      @data.push({name: section.pretty_name, data: data})
+      @attendance_data.push({name: section.pretty_name, data: data})
+    end
+
+    @marks_data = []
+    sections.each do |section|
+      total_scores = Hash.new(0)
+      student_ids = students[section.id].collect(&:id)
+      exams.each do |exam|
+        if student_ids.include?(exam.student_id)
+          [:english, :hindi, :mathematics, :science, :social].each do |subject|
+            total_scores[subject] += exam.send(subject)
+          end
+        end
+      end
+      students_count = students[section.id].length
+      avg_scores = {}
+      total_scores.each do |subject, total|
+        avg_scores[subject.to_s.capitalize] = total / students_count
+      end
+      @marks_data.push({name: section.pretty_name, data: avg_scores})
     end
   end
 end
